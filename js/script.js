87aa68f8ab8d451bcb95cd3da4f2d9ce5c62e514
maxzhiliaev <zilaevmaks057@gmail.com>
2025-07-15 13:12:33 +0300

    Hw

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const age = 16
+console.log(age)
+
+const myName = "Max"
+console.log(myName)
+
+const isStudent = true
+console.log(isStudent)
+
+const myString = "qwerty"
+console.log(myString)
+
+let myNumber = 25;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null
+console.log(myNull)
+
+let userName = prompt("Введіть своє ім’я");
+alert(userName);
+console.log(typeof userName);
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?")
+console.log(userAnswer)
+
+alert("Увага! Дія може бути небезпечною")
+let isSure = confirm("Ви впевнені, що хочете продовжити?")
+console.log(isSure)
